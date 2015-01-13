c27d709e8e7aaa75a632dd9d2a4e129ae3b639d4
Don Ricardo Jr <dricardo1@gmail.com>
2015-01-13 01:31:36 -0500

    Rf 1.9
    Grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,28 @@ grunt.initConfig({
       dest: 'build/css/styles.css',
     },
   },
+  watch: {
+ js: {
+    files: ['js/**/*.js'],
+    tasks: ['concat'],
+    },
+ css: {
+    files: ['css/**/*.css'],
+    tasks: ['concat'],
+    },
+},
+  uglify: {
+    options: {
+      mangle: false
+    },
+    my_target: {
+      files: {
+        'build/js/scripts.js': ['js/1.js', 'js/2.js']
+      }
+    }
+  }
 });	
-
 grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-uglify');
 };
