891e977b6883c5666c96df021eb209d74cbb15d1
Don Ricardo Jr <dricardo1@gmail.com>
2015-01-13 00:49:09 -0500

    Rf 1.6
    add Grunt pkgs. 1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,16 @@
 module.exports = function(grunt){
+// Project configuration.
+grunt.initConfig({
+  concat: {
+    options: {
+      separator: ';',
+    },
+    dist: {
+      src: ['js/1.js', 'js/2.js'],
+      dest: 'build/scripts.js',
+    },
+  },
+});	
 
 grunt.rloadNpmTasks('grunt-contrib-concat');
 };
