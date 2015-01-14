eead83b72624414c89a8ca32e8f13e0faa96c704
Don Ricardo Jr <dricardo1@gmail.com>
2015-01-13 23:45:02 -0500

    Rf 2.0 jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,4 +38,5 @@ grunt.initConfig({
 grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-jshint');
 };
