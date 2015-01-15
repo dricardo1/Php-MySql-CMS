7d802ce8ebce28e8ef4186402d0670a52bfbcc2e
Don Ricardo Jr <dricardo1@gmail.com>
2015-01-14 23:45:09 -0500

    Rf 2.1

--- today.php
+++ today.php
@@ -5,7 +5,7 @@
     <title>Today&rsquo;s Date</title>
   </head>
   <body>
-    <p>Today&rsquo;s time and date (according to this web server) is
+    <p>Today&rsquo;s time and date (according to this web server) is currently: 
       <?php
 
 
